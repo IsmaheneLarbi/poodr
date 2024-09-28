e2a1ae23d1fdc8e674a786e4dbd01c36fbe1f9a1
ismahene.larbi <ismahenelarbi@protonmail.com>
2024-09-28 21:58:38 +0200

    design methods to have a single responsibility

--- chapter2/revealing_references.rb
+++ chapter2/revealing_references.rb
@@ -1,11 +1,23 @@
+# example of OODR class
 class RevealingReferences
   attr_reader :wheels
+
   def initialize(data)
     @wheels = wheelify(data)
   end
 
-  wheel = Struct.new(:rim, :tire)
-  def wheelify(data)
-    data.collect 
+  def diameters
+    wheels.collect { |wheel| diameter(wheel) }
   end
-end
+
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
+  end
+
+
+# two dimensional array of rims and tires
+data = [[622, 20], [622, 23], [559, 30], [559, 40]]
+
+inst1 = RevealingReferences.new(data)
+diameters = inst1.diameters
+puts diameters[0]
