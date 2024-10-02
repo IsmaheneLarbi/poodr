95fe83a7866535b0c156014015f13e5abb96db91
ismahene.larbi <ismahenelarbi@protonmail.com>
2024-10-02 14:26:06 +0200

    dependency injection

---
+++ chapter3/wheel.rb
@@ -0,0 +1,20 @@
+# example of ood class
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (2 * tire)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+# wheel1 = Wheel.new(26, 1.5)
+# puts wheel1.circumference
