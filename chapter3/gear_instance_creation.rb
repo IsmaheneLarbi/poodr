9e2caaf9f86ee2ba161409c006cc4568c0f486d2
ismahene.larbi <ismahenelarbi@protonmail.com>
2024-10-02 14:27:36 +0200

    isolate instance creation in the Gear class

---
+++ chapter3/gear_instance_creation.rb
@@ -0,0 +1,21 @@
+require_relative 'wheel'
+
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+end
+
+g1 = Gear.new(52, 11, Wheel.new(26, 1.5))
+puts g1.gear_inches
